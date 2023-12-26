51a30f648d7947f99a0247be6b871110536adfd4
Roger Oliveira <41457384+Rjj18@users.noreply.github.com>
2023-12-26 19:32:25 +0000

    update profile info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
+  name: "Roger Oliveira",
+  title: "Web Developer & Content Creator",
   email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  gitHub: "https://github.com/Rjj18",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/roger-oliveira-4b3a04110/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
